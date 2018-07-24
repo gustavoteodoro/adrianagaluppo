92b8a71d37b2b0841e8d2431c83281addfdb760e
Gustavo Teodoro <gustavoalvesteodoro@gmail.com>
2018-07-24 00:03:14 -0300

    Update contentfulKeys.js

--- src/utils/consts/contentfulKeys.js
+++ src/utils/consts/contentfulKeys.js
@@ -1,2 +1,2 @@
-export const space = 'SPACE';
-export const accessToken = 'ACESSTOKEN';
+export const space = 'd8j81amy65r8';
+export const accessToken = 'a4dfdd6bd0ab3d7fae5efc6223906bdb3420a4991180519d69dbb8046c92b222';
