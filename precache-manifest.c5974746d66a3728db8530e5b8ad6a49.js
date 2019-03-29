c5ab61f8409f85429f680c14b161b34e6f18f50e
Gustavo Teodoro <gustavoalvesteodoro@gmail.com>
2019-03-29 08:38:22 -0300

    Updates

---
+++ precache-manifest.c5974746d66a3728db8530e5b8ad6a49.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3ca07f53d6cf4f4acad5",
+    "url": "/static/js/main.6c55796f.chunk.js"
+  },
+  {
+    "revision": "0944449a11335a5be422",
+    "url": "/static/js/2.342f62da.chunk.js"
+  },
+  {
+    "revision": "f03823bd285fa8612610d1f7d7e7cabd",
+    "url": "/index.html"
+  }
+];
